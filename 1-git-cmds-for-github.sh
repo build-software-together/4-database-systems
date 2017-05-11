3919ce1f0cab5e9c974cadb6db9d7c1d5f94cc34
Sakibur-Rahman <githubbangladesh@gmail.com>
2017-05-11 18:17:40 +0600

    cse301, summer2017, lab-01

---
+++ 1-git-cmds-for-github.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "cse301, summer2017, lab-01"
+git remote add origin https://github.com/githubBangladesh/DatabaseSystems
+git remote -v
+git push origin master
+git push --all -f https://github.com/githubBangladesh/DatabaseSystems
--- InventoryDB.sql
+++ InventoryDB.sql
@@ -1,6 +1,28 @@
-// Database Name: InventoryDB
+########################
+### CRUD Operations ####
+########################
+// Create  - Done
+// Retrive - Done
+// Update  - 
+// Delete  - 
+########################
 
-// Item Table
+
+// Course : CSE301, DatabaseSystems
+// Lab  : 01
+// Dated: 11-05-2017
+
+
+
+###################################
+### Database Name: InventoryDB ####
+###################################
+
+####################
+### Create Table ###
+####################
+
+// Item
 CREATE TABLE  `InventoryDB`.`Item` (
 `Name` VARCHAR( 50 ) NOT NULL ,
 `Manufacturer` VARCHAR( 40 ) NOT NULL ,
@@ -8,8 +30,55 @@ CREATE TABLE  `InventoryDB`.`Item` (
 `UnitPrice` DOUBLE NOT NULL
 ) ENGINE = MYISAM ;
 
-// SoldQuantity Table
+// SoldQuantity
 CREATE TABLE  `InventoryDB`.`SoldItem` (
 `Name` VARCHAR( 50 ) NOT NULL ,
 `SoldQuantity` INT( 20 ) NOT NULL
 ) ENGINE = MYISAM ;
+
+
+###################
+### Insert Data ###
+###################
+// Item
+insert into item values ('Potato', 'Bombay Sweets', 20, 15.00);
+insert into item values ('Cocacola 1 Ltr', 'Coca Cola', 50, 35);
+insert into item values ('Ice-cream 1.5 Ltr', 'Igloo', 10, 150);
+
+// SoldItem
+insert into solditem values ('Potato', 2);
+insert into solditem values ('Cocacola 1 Ltr', 2);
+
+#####################
+### Retrive Data ####
+#####################
+select <<column_name>>
+from <<table_name>>
+where <<condition>>
+
+// examples
+select * 
+from item
+where quantity > 10;
+
+
+### Multi-Realation Queries ##
+##############################
+
+// ex: ...retrive/display data...all matched columns : natural join
+select *
+from item, solditem
+where item.name = solditem.name;
+
+
+// ex: ...retrive/display data...required number of columns
+select item.name, item.unitprice, solditem.soldquantity
+from item, solditem
+where item.name = solditem.name
+
+// ex: ...retrive/display data...required number of columns
+select item.name, item.unitprice, solditem.soldquantity, item.unitprice*solditem.soldquantity
+from item, solditem
+where item.name = solditem.name
+
+
